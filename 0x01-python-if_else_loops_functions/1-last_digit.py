483d36c196d9e81b38d5b9f6b38cb639eb748780
Thulani <nalebaniokiy99@gmail.com>
2022-05-31 05:06:10 -0700

    aa

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number > 5:
+    print(f"the string and {number} is greater than 5")
+if number == 0:
+    print(f"the string {number} and is 0")
+if number < 6 != 0:
+    print(f"the string and {number} is less than 6 and not 0\n")
