bbddf2c93db06bfb5c5f3f0bb2e68533eb5a024e
Thulani <nalebaniokiy99@gmail.com>
2022-05-31 08:26:36 -0700

    aa

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,16 +1,3 @@
 #!/usr/bin/python3
-def lowercaseAlphabets():
-    for c in range(97, 123):
-        print(chr(c), end = " ")
-    print("")
-
-def uppercaseAlphabets():
-    for c in range(65, 91):
-        print(chr(c), end = " ")
-    print("")
-
-print("Uppercase Alphabets")
-uppercaseAlphabets()
-
-print("Lowercase Alphabets ")
-lowercaseAlphabets()
+for i in range(97, 123):
+    print("{:c}".format(i), end='')
