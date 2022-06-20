ada10cfc8921ee9a92024127add602ab21293dd5
Thulani <nalebaniokiy99@gmail.com>
2022-06-20 12:26:43 -0700

    0x05. Python - Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    try:
-        num = 0
-        for i in my_list:
-            if num < x:
-                print("{}".format(i), end='')
-                num += 1
-        print('')
-        return num
-    except SyntaxError:
-        print("Error Syntax")
+    y = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end='')
+            y += 1
+        except IndexError:
+            break
+    print()
+    return y
